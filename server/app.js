d4783cce11d88cfd8af33a1298e1914334cdfbf0
Viiuska <87257685+Viiuska@users.noreply.github.com>
2026-01-21 08:21:43 +0200

    first commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const port = 5000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname,"public")));
+
+
+app.listen(port, () =>{
+    console.log("server started");
+});
